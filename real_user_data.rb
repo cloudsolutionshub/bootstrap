65b20ef8b1ef09598c3906edcf62573dfa7f88fb
Saad Masood <saad@cloudsolutions.pk>
2016-08-26 16:45:19 +0500

    initial version of real user data. install update and install chef.
    initial version of real user data. install update and install chef.

---
+++ real_user_data.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+require 'optparse'
+require 'open3'
+options = {}
+options_parser = OptionParser.new do |opts|
+  opts.on("-r role", "--role=role") do |role|
+    options[:role] = role
+  end
+
+  opts.on("-e env_name", "--environment=name") do |env|
+    options[:environment]  = env
+  end
+
+  opts.on("--install-packages=", Array) do |packages|
+    options[:packages] = packages
+  end
+end
+
+options_parser.parse!
+puts options.inspect
+options[:packages] ||= []
+options[:packages] << 'epel-release'
+
+#install default packages.
+options[:packages].each do |pack|
+  if !system("yum install -y #{pack}")
+    STDERR.puts "Could not install #{pack} package. Aborting"
+    exit 1
+  end
+end
+#install chef
+output, error, status = Open3.capture3("curl -L https://omnitruck.chef.io/install.sh | sudo bash")
+if status == 0
+  puts output
+  puts "Chef Installed"
+else
+  STDERR.puts "Chef Installation failed:"
+  STDERR.puts error
+end
